7c50400be0a599e4c7866b47b361649bbed5d1fe
Gamearoo <gamearoo10@gmail.com>
2021-12-20 21:19:29 -0500

    Update BlockInit.java

--- src/main/java/com/extrablocksmod/gamearoo/init/BlockInit.java
+++ src/main/java/com/extrablocksmod/gamearoo/init/BlockInit.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.extrablocksmod.gamearoo.objects.blocks.BlockBase;
 
+
 import java.util.ArrayList;
 
 import net.minecraft.block.Block;
@@ -12,4 +13,5 @@ import net.minecraft.block.material.Material;
 public class BlockInit {
 	public static final List<Block> BLOCKS = new ArrayList<Block>();
 	public static final Block RUBY_BLOCK = new BlockBase("ruby_block", Material.GLASS);
+	
 }
